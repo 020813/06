f219040a2648ef2fcbe679cef9f3cb58faf44aec
020813 <113406823+020813@users.noreply.github.com>
2022-10-12 16:07:20 +0900

    05

--- main.c
+++ main.c
@@ -3,18 +3,47 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void square(int a)
+void func1(int x)
+int combination (int n, int r)
 {
-	a = a*a;
+    int n,r;
+	int com = n!/(n-r)!r!;
+	
+	printf("func1 x is at %p\n", &x);
+	return (com);
+}
+
+int factorial (int n)
+ {
+	int i;
+	int res = 1;
+	for(i=0;i<n;i**)
+	return res;
+}
+
+int get_integer (int x)
+ {
+	printf("The answer is : ");
+	scanf("%d", &x);
+	return(com);
 }
 
 int main(int argc, char *argv[]) 
 {
-	int a=2;
+	int x;
+	int n,r;
+	int res_combination, res_factorial, res_get_integer;
 	
-	square (a);
+	res_combination = combination(n,r);
+	res_factorial = factorial(n,r);
+	res_get_integer = get_integer(n,r);
 	
-	printf("a=%i\n", a);
+	printf("%i %i %i\n", res_combination, res_factorial, res_get_integer);
+
+	printf("main x is at %p\n", &x);
+	func(x);
+	combination(2,3);
+
 	
 	return 0;
-}
+ }
